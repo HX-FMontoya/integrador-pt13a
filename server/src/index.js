112df83b8bcfd6a43f79a9db644b10327e317162
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-06-15 18:14:46 -0500

    hw promises done

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const data = require("./utils/data");
+const getCharById = require("./controllers/getCharById");
 const PORT = 3001;
 
 http
@@ -7,26 +7,10 @@ http
     res.setHeader("Access-Control-Allow-Origin", "*");
     const { url } = req;
     if (url.includes("/rickandmorty/character")) {
-      // /rickandmorty/character/71
-      // [] -> "7" -> 7
       const id = Number(url.split("/").at(-1));
-      // [{},{},{}]
-      const personaje = data.find((personaje) => personaje.id === id);
-      // personaje -> {}
-      if (personaje) {
-        // personaje -> objeto
-        res.writeHead(200, { "Content-Type": "application/json" });
-        //
-        res.end(JSON.stringify(personaje));
-      } else {
-        // personaje -> objeto
-        res.writeHead(404, { "Content-Type": "application/json" });
-        //
-        res.end(JSON.stringify({ message: "Character not found" }));
-      }
+      getCharById(res, id);
     } else {
       res.writeHead(403, { "Content-Type": "application/json" });
-      //
       res.end(JSON.stringify({ message: "Route not found" }));
     }
   })
