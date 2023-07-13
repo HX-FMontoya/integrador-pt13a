05ba8b8cbb6d93a895de469f60b258690b55e131
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-07-13 18:21:41 -0500

    hw seq part 1 done

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 const { PORT } = process.env;
 const server = require("./app");
+const { conn } = require("./DB_connection");
 
-server.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`);
-});
+conn
+  .sync({ force: false })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
